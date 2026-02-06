a7e4f5774ff5bb08744153aa6a702b898f4ac1a3
MuhammadFarid956 <mhmmdfaridirfn@gmail.com>
2026-02-06 10:53:05 +0700

    .

--- bank_accounts.py
+++ bank_accounts.py
@@ -38,6 +38,28 @@ class BankAccount:
             account.deposit(amount)
             print(f"\Transfer Complete!\n\n**********")
         except BalanceException as error:
-            print(f"\Transfer Interrupted: {error}")
+            print(f"\nTransfer Interrupted: {error}")
+
+class InterestRewardAcct(BankAccount):
+    def deposit(self,amount):
+        self.balance = self.balance + (amount * 1.05)
+        print("\nDeposit Complete! (with 5% interest reward)")
+        self.getBalance()
+
+class SavingsAcct(BankAccount):
+    def __init__(self, initialAmount, acctName):
+        super().__init__(initialAmount, acctName)
+        self.fee = 5
+
+    def withdraw(self, amount):
+        try:
+            self.viableTransaction(amount + self.fee)
+            self.balance -= (amount + self.fee)
+            print(f"\nWithdraw Complete! (including ${self.fee} fee)")
+            self.getBalance()
+        except BalanceException as error:
+            print(f"\nWithdraw Interrupted: {error}")
+
+
              
       
--- oop_project.py
+++ oop_project.py
@@ -11,6 +11,14 @@ Udin.deposit(500)
 Udin.withdraw(500)
 
 Udin.transfer(50, Asep)
-# Udin.getBalance()
-# Asep.getBalance()
 
+Ahmad = InterestRewardAcct(3000, "Ahmad")
+Ahmad.deposit(1000)
+Ahmad.getBalance()
+Ahmad.transfer(500, Asep)
+
+Owi = SavingsAcct(1500, "Owi")
+Owi.getBalance()
+Owi.deposit(1000)
+Owi.withdraw(200)
+Owi.transfer(1000, Asep)
